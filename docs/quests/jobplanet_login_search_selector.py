22ff027420cefc46b80388a4c78117f5ab369526
nohjuhyeon <njh2720@gmail.com>
2023-12-22 17:46:41 +0900

    dsf

--- docs/quests/jobplanet_login_search_selector.py
+++ docs/quests/jobplanet_login_search_selector.py
@@ -18,10 +18,11 @@ html = browser.page_source                                                  # - 
 from selenium.webdriver.common.by import By                                 # - 정보 획득
 # browser.save_screenshot('./formats.png')           
 element_login_field = browser.find_element(by=By.CSS_SELECTOR,value="#user_email")                                                                  # 이메일 입력란 정보 수집
-element_login_field.send_keys("njh2720@gmail.com")                                                                                                  # 이메일 웹페이지에 전송
+email = input("E-MAIL : ")
+element_login_field.send_keys(email)                                                                                                  # 이메일 웹페이지에 전송
 
 element_password = browser.find_element(by=By.CSS_SELECTOR,value="#user_password")                                                                  # 비밀번호 입력란 정보 수집
-password = input("비밀번호 : ")                                                                                                                      # 비밀번호 입력
+password = input("PASSWORD : ")                                                                                                                      # 비밀번호 입력
 element_password.send_keys(password)                                                                                                                # 웹페이지에 전송
 element_login_button = browser.find_element(by=By.CSS_SELECTOR,value=" div > section.section_email.er_r > fieldset > button")                       # 로그인 버튼 정보 수집
 element_login_button.click()                                                                                                                        # 로그인 버튼 클릭
